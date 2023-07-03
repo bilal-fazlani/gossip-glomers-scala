4b86b4242ece163d08f24df7821791dd6db60726
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-07-03 10:00:28 +0530

    copy efficient broadcast 1

--- build.sbt
+++ build.sbt
@@ -1,20 +1,38 @@
 import sbtwelcome._
 import scala.sys.process._
 
-ThisBuild / scalaVersion := "3.3.0"
-ThisBuild / organization := "com.gossip-glomers"
-ThisBuild / resolvers ++= Resolver.sonatypeOssRepos("snapshots")
-ThisBuild / scalacOptions += "-Wunused:all"
-ThisBuild / version := "0.1.0-SNAPSHOT"
-ThisBuild / run / connectInput := true
-ThisBuild / nativeImageGraalHome := file(
-  "/Library/Java/JavaVirtualMachines/graalvm-community-openjdk-20.0.1+9.1/Contents/Home"
-).toPath
-ThisBuild / nativeImageAgentOutputDir := baseDirectory.value / "src" / "main" / "resources" / "META-INF" / "native-image"
-ThisBuild / nativeImageAgentMerge := true
-ThisBuild / nativeImageInstalled := true
-
 logo := ""
+
+lazy val commonSettings = Seq(
+  scalaVersion := "3.3.0",
+  organization := "com.gossip-glomers",
+  resolvers ++= Resolver.sonatypeOssRepos("snapshots"),
+  scalacOptions += "-Wunused:all",
+  version := "0.1.0-SNAPSHOT",
+  run / connectInput := true,
+  nativeImageGraalHome := file(
+    "/Library/Java/JavaVirtualMachines/graalvm-community-openjdk-20.0.1+9.1/Contents/Home"
+  ).toPath,
+  nativeImageAgentOutputDir := baseDirectory.value / "src" / "main" / "resources" / "META-INF" / "native-image",
+  nativeImageAgentMerge := true,
+  nativeImageInstalled := true,
+  libraryDependencies += "com.bilal-fazlani" %% "zio-maelstrom" % "0.4.1",
+  nativeImageOutput := baseDirectory.value / name.value,
+  logo := "",
+  bootstrap := {
+    Process(
+      Seq(
+        "coursier",
+        "bootstrap",
+        "--standalone",
+        s"com.gossip-glomers:${name.value}_3:0.1.0-SNAPSHOT",
+        "-f",
+        "-o",
+        s"${name.value}.jar"
+      )
+    ).!
+  }
+)
 
 usefulTasks := Seq(
   UsefulTask(
@@ -30,26 +48,10 @@ usefulTasks := Seq(
 lazy val bootstrap = taskKey[Unit]("Create a fat jar file")
 lazy val runSimulation = taskKey[Unit]("run maelstrom simulation to generate graalvm agent configuration")
 
-ThisBuild / bootstrap := {
-  Process(
-    Seq(
-      "coursier",
-      "bootstrap",
-      "--standalone",
-      s"com.gossip-glomers:${name.value}_3:0.1.0-SNAPSHOT",
-      "-f",
-      "-o",
-      s"${name.value}.jar"
-    )
-  ).!
-}
-
 lazy val efficientBroadcast1 = project
   .in(file("./efficient-broadcast-1"))
   .settings(
     name := "efficient-broadcast-1",
-    libraryDependencies += "com.bilal-fazlani" %% "zio-maelstrom" % "0.4.1",
-    nativeImageOutput := baseDirectory.value / name.value,
     Compile / mainClass := Some("gossipGlomers.EfficientBroadcast1"),
     runSimulation := {
       Process(
@@ -71,3 +73,31 @@ lazy val efficientBroadcast1 = project
     }
   )
   .enablePlugins(NativeImagePlugin)
+  .settings(commonSettings: _*)
+
+lazy val efficientBroadcast2 = project
+  .in(file("./efficient-broadcast-2"))
+  .settings(
+    name := "efficient-broadcast-2",
+    Compile / mainClass := Some("gossipGlomers.EfficientBroadcast2"),
+    runSimulation := {
+      Process(
+        Seq(
+          "maelstrom",
+          "test",
+          "-w",
+          "broadcast",
+          "--bin",
+          s"run.sh ${baseDirectory.value}/${name.value}.jar",
+          "--node-count",
+          "1",
+          "--time-limit",
+          "5",
+          "--rate",
+          "10"
+        )
+      ).!
+    }
+  )
+  .enablePlugins(NativeImagePlugin)
+  .settings(commonSettings: _*)
---
+++ efficient-broadcast-2/src/main/scala/gossipGlomers/Main.scala
@@ -0,0 +1,109 @@
+package gossipGlomers
+
+import zio.*
+import zio.json.*
+import com.bilalfazlani.zioMaelstrom.*
+import com.bilalfazlani.zioMaelstrom.protocol.*
+
+object EfficientBroadcast2 extends ZIOAppDefault {
+
+  enum NodeRole:
+    case Leader(followers: Set[NodeId])
+    case Follower(of: NodeId)
+
+  case class State(
+      role: NodeRole,
+      numbers: Set[Int] = Set.empty,
+      currentMessageId: MessageId = MessageId(0)
+  ) {
+    def addNumbers(ns: Set[Int]): State = copy(numbers = numbers ++ ns)
+    def addNumber(n: Int): State = copy(numbers = numbers + n)
+    def incMessageId = copy(currentMessageId = currentMessageId.inc)
+  }
+
+  private def nextMsgId = ZIO.serviceWithZIO[Ref[State]](_.updateAndGet(_.incMessageId).map(_.currentMessageId))
+
+  def gossipToLeader(leader: NodeId, number: Int) =
+    val retryCount = 10
+    (
+      for
+        nextMessageId <- nextMsgId
+        state <- ZIO.serviceWithZIO[Ref[State]](_.get)
+        _ <- leader
+          .ask[GossipOk](Gossip(number, nextMessageId), 2.seconds)
+          .retry(Schedule.recurs(retryCount))
+          .tapError(e => logError(s"gossip ask failed after $retryCount retries: ${e}"))
+          .catchAll(_ => ZIO.unit)
+      yield ()
+    ).unit
+
+  def updateToFollowers(followers: Set[NodeId], numbers: Set[Int]) =
+    val retryCount = 10
+    (
+      for
+        nextMessageId <- nextMsgId
+        state <- ZIO.serviceWithZIO[Ref[State]](_.get)
+        _ <- ZIO.foreachPar(followers) { follower =>
+          follower
+            .ask[UpdateOk](Update(numbers, nextMessageId), 2.seconds)
+            .retry(Schedule.recurs(retryCount))
+            .tapError(e => logError(s"update ask failed after $retryCount retries: ${e}"))
+            .catchAll(_ => ZIO.unit)
+        }
+      yield ()
+    ).unit
+
+  def followerHandler(leader: NodeId) = receive[FollowerInMessage] {
+    case Broadcast(number, msg_id) =>
+      ZIO.serviceWithZIO[Ref[State]](_.update(_.addNumber(number))) *>
+        (reply(BroadcastOk(msg_id)) zipPar gossipToLeader(leader, number))
+
+    case Update(numbers, msgId, _) =>
+      ZIO.serviceWithZIO[Ref[State]](_.update(_.addNumbers(numbers))) *> reply(UpdateOk(msgId))
+
+    case Read(msg_id) =>
+      ZIO.serviceWithZIO[Ref[State]](_.get.flatMap(state => reply(ReadOk(msg_id, state.numbers))))
+
+    case Topology(_, msg_id) => reply(TopologyOk(msg_id))
+  }
+
+  def leaderHandler(followers: Set[NodeId]) = receive[LeaderInMessage] {
+    case Broadcast(number, msg_id) =>
+      ZIO.serviceWithZIO[Ref[State]](_.update(_.addNumber(number))) *>
+        reply(BroadcastOk(msg_id)) zipPar updateToFollowers(followers, Set(number))
+
+    case Gossip(numbers, msg_id, _) =>
+      ZIO.serviceWithZIO[Ref[State]](_.update(_.addNumbers(numbers))) *>
+        (reply(GossipOk(msg_id)) zipPar updateToFollowers(followers - src, numbers))
+
+    case Read(msg_id) =>
+      ZIO.serviceWithZIO[Ref[State]](_.get.flatMap(state => reply(ReadOk(msg_id, state.numbers))))
+
+    case Topology(_, msg_id) => reply(TopologyOk(msg_id))
+  }
+
+  def handler = for
+    role <- ZIO.serviceWithZIO[Ref[State]](_.get.map(_.role))
+    _ <- role match
+      case NodeRole.Leader(followers) => leaderHandler(followers)
+      case NodeRole.Follower(leader)  => followerHandler(leader)
+  yield ()
+
+  val decideRole =
+    given Ordering[NodeId] = Ordering.by[NodeId, String](_.toString)
+    for
+      _ <- logDebug("deciding first alphabetical node as leader")
+      others <- getOtherNodeIds
+      myId <- getMyNodeId
+      leader = (others + myId).toSeq.sorted.head
+      leaderInfo = if leader == myId then NodeRole.Leader(others) else NodeRole.Follower(leader)
+      _ <- leaderInfo match
+        case NodeRole.Leader(_)        => logInfo(s"I am the leader ($myId)!")
+        case NodeRole.Follower(nodeId) => logInfo(s"leader is $nodeId")
+    yield leaderInfo
+
+  def run = handler.provideSome[Scope](
+    MaelstromRuntime.live(Settings(logLevel = NodeLogLevel.Info)),
+    ZLayer.fromZIO(decideRole).flatMap(role => ZLayer.fromZIO(Ref.make(State(role.get))))
+  )
+}
